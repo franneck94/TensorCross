8a3f2d2df8a6c4c26c5bfb9a7676d13c4e191e35
Saif Al-Dilaimi <Saif.Al-Dilaimi@rub.de>
2020-11-20 18:56:59 +0100

    added file

---
+++ tensorcross/grid_search.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+
+from typing import Any, Callable, Dict
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import ParameterGrid
+
+
+class GridSearch:
+    def __init__(
+        self,
+        model_fn: Callable,
+        parameter_grid: Dict[str, Any]
+    ) -> None:
+        self.model_fn = model_fn
+        self.parameter_grid = parameter_grid
+        self.results = {
+            "best_score": -np.inf,
+            "best_params": {},
+            "val_scores": [],
+            "params": [],
+        }
+
+
+    def fit(
+        self,
+        train_dataset: tf.data.Dataset,
+        val_dataset: tf.data.Dataset = None
+    ) -> None:
+        pass
