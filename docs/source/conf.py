44c73c1816d6299ef9ce7fb39eec9701a1bded5b
Jan Schaffranek <jan.schaffranek@rub.de>
2020-11-20 20:03:43 +0100

    Updated doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@ sys.path.insert(0, os.path.abspath('../../tensorcross/'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'tensorcross'
+project = 'TensorCross'
 copyright = '2020, Jan Schaffranek'  # pylint: disable=W0622
 author = 'Jan Schaffranek'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.1.0b'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/source/modules.rst
+++ docs/source/modules.rst
@@ -1,5 +1,5 @@
 tensorcross
-=========
+===========
 
 .. toctree::
    :maxdepth: 4
